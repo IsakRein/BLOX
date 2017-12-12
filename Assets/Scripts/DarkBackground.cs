6f11fefb25f56ee7d2841394ae084ffbdfb7c1fe
Isak Reinholdsson <isak.reinholdsson@gmail.com>
2017-12-12 11:00:21 +0100

    41

--- Assets/Scripts/DarkBackground.cs
+++ Assets/Scripts/DarkBackground.cs
@@ -4,13 +4,11 @@ using UnityEngine;
 
 public class DarkBackground : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private void Start()
+    {
+    }
+
+    void end () {
+        gameObject.SetActive(false);
+    }
 }
