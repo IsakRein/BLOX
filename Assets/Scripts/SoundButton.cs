f19427f5f565e795573191acff0d9761133278d0
Isak Reinholdsson <isak.reinholdsson@gmail.com>
2017-12-18 20:53:13 +0100

    49

---
+++ Assets/Scripts/SoundButton.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundButton : MonoBehaviour {
+
+	public Color backColor1;
+	public Color backColor2;
+
+	public GameObject soundOn;
+	public GameObject soundOff;
+
+	public bool colorSwitch;
+
+	public Image backSpr;
+
+	void Start() {
+		OnClick ();
+	}	
+
+	void OnClick() {
+		colorSwitch = !colorSwitch;
+
+		if (colorSwitch) {
+			soundOn.SetActive (true);
+			soundOff.SetActive (false);
+
+			backSpr.color = backColor1;
+
+			Vibrate ();
+		} else {
+			soundOn.SetActive (false);
+			soundOff.SetActive (true);
+
+			backSpr.color = backColor2;
+		}
+	}
+
+	void Vibrate() {
+		Handheld.Vibrate ();
+	}
+}
