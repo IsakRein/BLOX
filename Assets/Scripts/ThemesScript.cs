3b9f24820884ae95f1b24f7658609ea6dd53c127
Isak Reinholdsson <isak.reinholdsson@gmail.com>
2018-03-12 23:19:51 +0100

    78

--- Assets/Scripts/ThemesScript.cs
+++ Assets/Scripts/ThemesScript.cs
@@ -6,6 +6,8 @@ using UnityEngine.SceneManagement;
 public class ThemesScript : MonoBehaviour {
 
     public List<GameObject> themes = new List<GameObject>();
+    public List<Transform> chosens = new List<Transform>();
+
 
 	private void Start()
 	{
@@ -17,19 +19,25 @@ public class ThemesScript : MonoBehaviour {
         transform.GetChild(Manager.selectedTheme - 1).Find("Chosen").gameObject.SetActive(true);
 	}
 
+    void IdentifyChosen()
+    {
+        foreach (Transform child in transform)
+        {
+            chosens.Add(child.Find("Chosen"));
+        }
+    }
+
 	public void ChangeTheme(int themeNum)
     {
         Manager.selectedTheme = themeNum;
         PlayerPrefs.SetInt("Theme", themeNum);
 
-        foreach (Transform child in transform)
+        foreach (Transform child in chosens)
         {
-            child.Find("Chosen").gameObject.SetActive(false);
+            child.gameObject.SetActive(false);
         }
 
         transform.GetChild(themeNum - 1).Find("Chosen").gameObject.SetActive(true);
-
-
 
         //load scene
         string sceneToLoad = Manager.sceneOrder[Manager.sceneOrder.Count - 1];
